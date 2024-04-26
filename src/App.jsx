391d6b47e718e68694fbaa0ff19a73907d6f487e
jasnmar <jasnmar@gmail.com>
2024-04-26 10:35:15 -0400

    Passing function

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,13 @@
-import { useEffect, useRef, useState } from 'react'
+//import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import Decision from "./Decision"
 
 function App() {
-  const [text, setText] = useState("")
-  const [list, setList] = useState([])
-  const inputRef = useRef(null)
-
-  function handleChange(e) {
-    setText(e.target.value)
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    if (!text) {
-      return;
-    }
-    setList(prevList => [...prevList, text])
-    setText("")
-    inputRef.current.focus()
-  }
-  
-  return (
-    <>
-      <h2>React Project Ideas</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={handleChange}
-          value={text}
-          placeholder="Idea"
-          ref={inputRef}
-        />
-        <button>Submit</button>
-      </form>
-
-      <ol>
-        {list.map((item, i) => <li key={i}>{item}</li>)}
-      </ol>
-    </>
-  )
+    return (
+        <div>
+            <Decision sayName={function() { console.log('Hi Bob')}} />
+        </div>
+    )
 }
 
 export default App
---
+++ src/Decision.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+export default function Decision({ sayName }) {
+    const [goingOut, setGoingOut] = useState(false)
+    sayName()
+    function toggleGoingOut() {
+        setGoingOut(prev => !prev)
+    }
+    
+    return (
+        <div>
+            <button onClick={toggleGoingOut}>Change mind</button>
+            <h1>Am I going out tonight, {name}?? {goingOut ? "Yes!" : "Nope..."}</h1>
+        </div>
+    )
+}
